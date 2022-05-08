348fb4356f1dcd877298a82337e1ce4d6aca9de4
Lipe-pard <pardinhorh@gmail.com>
2022-05-08 20:07:56 -0300

    Add config product-api

---
+++ controllers/ProductController.php
@@ -0,0 +1,94 @@
+<?php 
+
+class ProductController{
+    function create(){
+        $response = new Output();
+        $response->allowedMethod('POST');
+        //Adicionar a variavel os atributos da classe
+        $name = $_POST['name'];
+        $discription = $_POST['discription'];
+        $value = $_POST['value'];
+        $amount = $_POST['amount'];
+        $event = $_POST['event'];
+        $type = $_POST['type'];
+        $color = $_POST['color'];
+        $image = $_POST['image'];
+        //Processamento
+        $product = new Product(NULL, $name, $discription, $value, $amount, $event, $type, $color, $image);
+        $id = $product->create();
+        //Saida
+        $result['Mensage'] = "Produto Criado com sucesso";
+        $result['product']['id'] = $id;
+        $result['product']['name'] = $name;
+        $result['product']['discription'] = $discription;
+        $result['product']['value'] = $value;
+        $result['product']['amount'] = $amount;
+        $result['product']['event'] = $event;
+        $result['product']['type'] = $type;
+        $result['product']['color'] = $color;
+        $result['product']['image'] = $image;
+        $response = new Output();
+        $response->out($result);
+    }
+
+    function delete(){
+        $response = new Output();
+        $response->allowedMethod('POST');
+        $id = $_POST['id'];
+        $product = new Product($id, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL);
+        $product->delete();  
+        $result['Mensage'] = "Deletado com Sucesso";
+        $result['user']['id'] = $id;
+        $response = new Output();
+        $response->out($result);
+    }
+
+    function update(){
+        $response = new Output();
+        $response->allowedMethod('POST');
+        $id = $_POST['id'];
+        $name = $_POST['name'];
+        $discription = $_POST['discription'];
+        $value = $_POST['value'];
+        $amount = $_POST['amount'];
+        $event = $_POST['event'];
+        $type = $_POST['type'];
+        $color = $_POST['color'];
+        $image = $_POST['image'];
+        $product = new Product($id, $name, $discription, $value, $amount, $event, $type, $color, $image);        
+        $product->update();
+        $result['Mensage'] = "Editado com sucesso";
+        $result['product']['id'] = $id;
+        $result['product']['name'] = $name;
+        $result['product']['discription'] = $discription;
+        $result['product']['value'] = $value;
+        $result['product']['amount'] = $amount;
+        $result['product']['event'] = $event;
+        $result['product']['type'] = $type;
+        $result['product']['color'] = $color;
+        $result['product']['image'] = $image;
+        $response = new Output();
+        $response->out($result);
+    }
+
+    function selectAll(){
+        $response = new Output();
+        $response->allowedMethod('GET');
+        $product = new Product(NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL);
+        $result = $product->selectAll();
+        $response = new Output();
+        $response->out($result);
+    }
+
+    function selectId(){
+        $response = new Output();
+        $response->allowedMethod('GET');
+        $id = $_GET['id'];
+        $product = new Product($id, NULL, NULL, NULL, NULL, NULL, NULL, NULL);
+        $result = $product->selectId();
+        $response = new Output();
+        $response->out($result);
+    }
+}
+
+?>
---
+++ models/Product.php
@@ -0,0 +1,123 @@
+<?php 
+
+class Product{
+    //prioridades
+    public $id;
+    public $name;
+    public $description;
+    public $value;
+    public $amount;
+    public $event;
+    public $type;
+    public $color;
+    public $image;
+
+    //Construct
+    function __construct($id, $name, $discription, $value, $amount, $event, $type, $color, $image){
+        $this->id = $id;
+        $this->name = $name;
+        $this->discription = $discription;
+        $this->value = $value;
+        $this->amount = $amount;
+        $this->event = $event;
+        $this->type = $type;
+        $this->color = $color;
+        $this->image = $image;
+    }
+
+    //Métodos
+    function create(){
+        $db = new Database();
+        try{
+            $stmt = $db->conn->prepare("INSERT INTO product (name, discription, value, amount, event, type, color, image) VALUES (:name, :discription, :value, :amount, :event, :type, :color, :image)");
+            $stmt->bindParam(":name", $this->name);
+            $stmt->bindParam(":discription", $this->discription);
+            $stmt->bindParam(":value", $this->value);
+            $stmt->bindParam(":amount", $this->amount);
+            $stmt->bindParam(":event", $this->event);
+            $stmt->bindParam(":type", $this->type);
+            $stmt->bindParam(":color", $this->color);
+            $stmt->bindParam(":image", $this->image);
+            $stmt->execute();
+
+            $id = $db->conn->lastInsertId();
+            return $id;
+
+        }catch(PDOException $e){
+            $result['Mensage'] = "Erro Create" . $e->getMessage();
+            $response = new Output();
+            $response->out($result, 500);
+        }
+    }
+
+    function delete(){
+        $db = new Database();
+        try{
+            $stmt = $db->conn->prepare("DELETE FROM product WHERE id = :id;");
+            $stmt->bindParam(":id", $this->id);
+            $stmt->execute();
+            return true; 
+        }catch(PDOException $e) {
+            $result['Mensage'] = "Erro no Delete" . $e->getMessage();
+            $response = new Output();
+            $response = out($result, 500);
+          }
+    }
+
+    function update(){
+        $db = new Database();
+        try {
+            $stmt = $db->conn->prepare("UPDATE product SET name=:name, discription=:discription, value=:value, amount=:amount, event=:event, type=:type, color=:color, image=:image WHERE id=:id;");
+            $stmt->bindParam(':id', $this->id);
+            $stmt->bindParam(':name', $this->name);
+            $stmt->bindParam(':discription', $this->discription);
+            $stmt->bindParam(':value', $this->value);
+            $stmt->bindParam(":amount", $this->amount);
+            $stmt->bindParam(':event', $this->event);
+            $stmt->bindParam(':type', $this->type);
+            $stmt->bindParam(':color', $this->color);
+            $stmt->bindParam(':image', $this->image);
+            $stmt->execute();
+            return true;
+          } 
+          catch(PDOException $e) {
+            $result['Mensage'] = "Erro de Update" . $e->getMessage();
+            $response = new Output();
+            $reaponse = out($result, 500);
+          }
+    }
+
+    function selectAll(){
+        $db = new Database();
+        try {
+        $stmt = $db->conn->prepare("SELECT * FROM product");
+        $stmt->execute();
+        $result = $stmt->fetchAll(PDO::FETCH_ASSOC);
+        return $result;
+
+        } 
+        catch(PDOException $e) {
+        $result['Mensage'] = "Erro de Select" . $e->getMessage();
+        $response = new Output();
+        $reaponse = out($result, 500);
+        }
+    }
+
+    function selectId(){
+        $db = new Database();
+        try {
+          $stmt = $db->conn->prepare("SELECT name, discription, value, amount, event, type, color, image  FROM product WHERE id = :id");
+          $stmt->bindParam(":id", $this->id);
+          $stmt->execute();
+          $result = $stmt->fetchAll(PDO::FETCH_ASSOC);
+          return $result;
+        } 
+        catch(PDOException $e) {
+          $result['Mensage'] = "Erro Select id" . $e->getMessage();
+          $response = new Output();
+          $reaponse = out($result, 500);
+        }
+    }
+}
+
+?>
